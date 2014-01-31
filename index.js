db27a1a26f9c3c996179125e3baa98a5457d004f
Noah H. Smith <noah@batteredoats.com>
2014-01-31 14:42:07 -0800

    add a friendly message

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('');
+  res.end('Hello good sir.');
 }).listen(PORT);
 console.log('Server running at http://127.0.0.1:' + PORT + '/');
